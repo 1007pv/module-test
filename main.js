292b3e87e469952a25adb3eaa8bb0f7aad2ffa69
Pankaj Verma <84775453+1007pv@users.noreply.github.com>
2022-12-30 23:50:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function submitForm() {
+    
+    let fname = document.getElementById('fname').value;
+    let lname = document.getElementById('lname').value;
+    let email = document.getElementById('email').value;
+    let reason = document.getElementById('reason').value;
+    console.log(fname, lname, email, reason);
+    if (fname != null && lname != null && email != null && reason != false) {
+         alert("Form submitted successfully!")
+    } else {
+        alert("All fields are required!")
+    }
+}
